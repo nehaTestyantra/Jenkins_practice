b3ecb088f0a68532084010eae581f0426ebb16a7
nehazutti-16 <neha.zutti@gmail.com>
2024-05-30 16:20:13 +0530

    commit

---
+++ Jenkin_practice/src/test/java/p1/pgm1Test.java
@@ -0,0 +1,15 @@
+package p1;
+
+import org.testng.annotations.Test;
+
+public class pgm1Test {
+
+	@Test 
+	public void pgmTest() {
+		System.out.println("----JENKINS-----");
+		
+		
+		System.out.println("JENKINS Executed");
+		
+	}
+}
